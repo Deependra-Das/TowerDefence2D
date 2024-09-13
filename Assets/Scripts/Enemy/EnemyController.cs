9e64de4996f1baddea0537609ea681b9e23aa931
Deependra-Das <iamdeep75@gmail.com>
2024-09-13 21:11:22 +0530

    Updated EnemyController
    - Created Separate functions for Finding Path, Enemy Movement, MovementAnimation & setting Direction for Animator

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -36,10 +36,21 @@ public class EnemyController : MonoBehaviour
 
     void Update()
     {
-        if(Vector2.Distance(destination.position, transform.position)<=0.1f)
+        FindPath();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        MovementAnimation();
+    }
+
+    private void FindPath()
+    {
+        if (Vector2.Distance(destination.position, transform.position) <= 0.1f)
         {
             pathIndex++;
-           
+
             if (pathIndex >= GameManager.Instance.path.Length)
             {
                 EnemySpawnManager.onEnemyDestroy.Invoke();
@@ -54,44 +65,41 @@ public class EnemyController : MonoBehaviour
         }
     }
 
-    private void FixedUpdate()
+    private void Move()
     {
         Vector2 direction = (destination.position - transform.position).normalized;
-        rb2D_Enemy.velocity= direction * currentSpeed;
+        rb2D_Enemy.velocity = direction * currentSpeed;
+    }
 
-        if((int)rb2D_Enemy.velocity.x>0 && (int)rb2D_Enemy.velocity.y==0)
+    private void MovementAnimation()
+    {
+        if ((int)rb2D_Enemy.velocity.x > 0 && (int)rb2D_Enemy.velocity.y == 0)
         {
-            enemyAnimator.SetBool("Right", true);
-            enemyAnimator.SetBool("Left", false);
-            enemyAnimator.SetBool("Top", false);
-            enemyAnimator.SetBool("Bottom", false);
+            SetDirectionForAnimation(true,false,false, false);
         }
 
         else if ((int)rb2D_Enemy.velocity.x < 0 && (int)rb2D_Enemy.velocity.y == 0)
         {
-            enemyAnimator.SetBool("Right", false);
-            enemyAnimator.SetBool("Left", true);
-            enemyAnimator.SetBool("Top", false);
-            enemyAnimator.SetBool("Bottom", false);
+            SetDirectionForAnimation(false, true, false, false);
         }
 
         else if ((int)rb2D_Enemy.velocity.x == 0 && (int)rb2D_Enemy.velocity.y > 0)
         {
-            enemyAnimator.SetBool("Right", false);
-            enemyAnimator.SetBool("Left", false);
-            enemyAnimator.SetBool("Top", true);
-            enemyAnimator.SetBool("Bottom", false);
+            SetDirectionForAnimation(false, false, true, false);
         }
 
         else if ((int)rb2D_Enemy.velocity.x == 0 && (int)rb2D_Enemy.velocity.y < 0)
         {
-            enemyAnimator.SetBool("Right", false);
-            enemyAnimator.SetBool("Left", false);
-            enemyAnimator.SetBool("Top", false);
-            enemyAnimator.SetBool("Bottom", true);
+            SetDirectionForAnimation(false, false, false, true);
         }
+    }
 
-
+    private void SetDirectionForAnimation(bool right, bool left, bool top, bool bottom)
+    {
+        enemyAnimator.SetBool("Right", right);
+        enemyAnimator.SetBool("Left", left);
+        enemyAnimator.SetBool("Top", top);
+        enemyAnimator.SetBool("Bottom", bottom);
     }
 
     public void TakeDamge(int damageValue)
