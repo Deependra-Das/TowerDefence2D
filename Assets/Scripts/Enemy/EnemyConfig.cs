e12078b4c9dfaf53e839abbccb0b483cdd076307
Deependra-Das <iamdeep75@gmail.com>
2024-09-13 22:13:02 +0530

    Updated EnemySpawnManager
    - Updated SpawnEnemy function
    - Removed Wave Index enum
    - Added separate script for EnemyWaveSystem class

---
+++ Assets/Scripts/Enemy/EnemyConfig.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyConfig : MonoBehaviour
+{
+    public enum EnemyType
+    {
+        SMALL,
+        MEDIUM,
+        TANK,
+    }
+}
